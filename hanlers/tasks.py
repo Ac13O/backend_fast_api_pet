259c53953798753f34d77a4a51024dc6b9df9829
Aleksander_Loktev <Aleksandr.Loktev@atom.team>
2025-06-02 22:19:44 +0300

    created handlers for tasks

--- hanlers/tasks.py
+++ hanlers/tasks.py
@@ -1,18 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fixtures import tasks as fixture_tasks
+from schema.task import Task
 
-router = APIRouter(prefix="/tasks", tags=["tasks_"])
-
-
-@router.get("/")
-async def simple():
-    return []
+router = APIRouter(prefix="/task", tags=["tasks_"])
 
 
-@router.get("/all")
+@router.get("/all", response_model=list[Task])
 async def get_tasks():
-    return {"tasks": "all"}
+    return fixture_tasks
 
 
-@router.post("/task/{taskid}")
-async def create_task(taskid):
-    return
+@router.post("/", response_model=Task)
+async def create_task(task: Task):
+    fixture_tasks.append(task)
+    return task
+
+
+@router.patch("/{task_id}", response_model=Task)
+async def update_task(task_id: int, new_name: str):
+    for task in fixture_tasks:
+        if task.get("id") == task_id:
+            task.update({"name": new_name})
+            return task
+
+
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(task_id: int):
+    for task in fixture_tasks:
+        if task.get("id") == task_id:
+            fixture_tasks.remove(task)
+            return f"Task with id = {task_id} was removed"
+    return "Task was not found"
