f958db8cdeafd15089864b00b5b208f7c12801ca
Aleksander_Loktev <Aleksandr.Loktev@atom.team>
2025-06-26 23:49:13 +0300

    reviewed handlers

--- hanlers/tasks.py
+++ hanlers/tasks.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status
-from fixtures import tasks as fixture_tasks
+
+from database import get_db_connection
 from schema.task import Task
 
 router = APIRouter(prefix="/task", tags=["tasks_"])
@@ -7,27 +8,49 @@ router = APIRouter(prefix="/task", tags=["tasks_"])
 
 @router.get("/all", response_model=list[Task])
 async def get_tasks():
-    return fixture_tasks
+    result = []
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    tasks = cursor.execute("SELECT * FROM Tasks").fetchall()
+    for task in tasks:
+        result.append(Task(
+            id=task[0],
+            name=task[1],
+            pomodoro_count=task[2],
+            category_id=task[3]
+        ))
+    connection.close()
+    return result
 
 
 @router.post("/", response_model=Task)
 async def create_task(task: Task):
-    fixture_tasks.append(task)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Tasks (name, pomodoro_count, category_id) VALUES (?, ?, ?)",
+                   (task.name, task.pomodoro_count, task.category_id))
+    connection.commit()
+    connection.close()
     return task
 
 
 @router.patch("/{task_id}", response_model=Task)
-async def update_task(task_id: int, new_name: str):
-    for task in fixture_tasks:
-        if task.get("id") == task_id:
-            task.update({"name": new_name})
-            return task
+async def update_task_name(task_id: int, new_name: str):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Tasks SET name=? WHERE id=?",
+                   (new_name, task_id))
+    connection.commit()
+    task = cursor.execute("SELECT * FROM Tasks WHERE id=?", f"{task_id}").fetchall()[0]
+    connection.close()
+    return Task(id=task[0], name=task[1], pomodoro_count=task[2], category_id=task[3])
 
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(task_id: int):
-    for task in fixture_tasks:
-        if task.get("id") == task_id:
-            fixture_tasks.remove(task)
-            return f"Task with id = {task_id} was removed"
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Tasks WHERE id=(?)", (str(task_id)))
+    connection.commit()
+    connection.close()
     return "Task was not found"
